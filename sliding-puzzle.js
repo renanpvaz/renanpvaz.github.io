113d21be9f2cbceea36d6867bd17ec0e2a6b2ea3
renanpvaz <renan.piresvz@gmail.com>
2017-10-05 19:50:11 -0300

    finally add some stuff to my website

---
+++ sliding-puzzle.js
@@ -0,0 +1,82 @@
+const slidingPuzzle = (target, size = 50) => {
+  const shuffle = arr => arr.slice().sort(() => Math.random() > .5)
+
+  const table = shuffle([
+    ['0 0', `-${size}px 0`, `-${size * 2}px 0`],
+    [`0 -${size}px`, `-${size}px`, `-${size * 2}px -${size}px`],
+    [`0 -${size * 2}px`, `-${size}px -${size * 2}px`, 0],
+  ]).map(shuffle)
+
+  Object.assign(target.style, {
+    width: `${size * 3}px`,
+    height: `${size * 3}px`
+  })
+
+  const appendTo = parent => child => parent.appendChild(child)
+
+  const render = table => (
+    table.forEach(
+      (column, y) => column
+      .map((value, x) => !!value ? createTile(value, x, y) : document.createElement('div'))
+      .forEach(appendTo(target))
+    )
+  )
+
+  const clear = element => Object.assign(element, { innerHTML: '' })
+
+  const get = table => (x, y) => table[y] != null ? table[y][x] : null
+
+  const canBeMoved = table => (x, y) => (
+    get(table)(x, y + 1) === 0 ||
+    get(table)(x, y - 1) === 0 ||
+    get(table)(x + 1, y) === 0 ||
+    get(table)(x - 1, y) === 0
+  )
+
+  const toPosition = value => parseInt(value) / size
+
+  const getEmptySpot = table => {
+    const row = table.find(row => row.includes(0))
+    return [row.indexOf(0), table.indexOf(row)]
+  }
+
+  const moveTile = tile => {
+    const { top, left } = tile.style
+    const [emptyX, emptyY] = getEmptySpot(table)
+    const x = toPosition(left)
+    const y = toPosition(top)
+
+    if (canBeMoved(table)(x, y)) {
+      table[emptyY][emptyX] = table[y][x]
+      table[y][x] = 0
+
+      clear(target)
+      render(table)
+    }
+  }
+
+  const createTile = (value, x, y) => {
+    const tile = document.createElement('figure')
+
+    tile.classList.add('sliding-puzzle__tile')
+    Object.assign(tile.style, {
+      width: `${size}px`,
+      height: `${size}px`,
+      top: `${size * y}px`,
+      left: `${size * x}px`,
+      backgroundPosition: value,
+      backgroundSize: `${size * 3}px`,
+    })
+
+    tile.addEventListener('click', e => moveTile(e.target))
+
+    return tile
+  }
+
+  render(table)
+}
+
+slidingPuzzle(
+  document.querySelector('.sliding-puzzle'),
+  40
+)
